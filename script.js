a29cd86346fefeb14d82bb838f8da112dcdb7084
shwetasharma1302 <126080003+shwetasharma1302@users.noreply.github.com>
2023-08-17 16:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+    const ropeLengthsInput = document.getElementById("ropeLengths").value;
+    const ropeLengthsArray = ropeLengthsInput.split(",").map(length => parseInt(length.trim(), 10));
+
+    const minCost = calculateMinimumCost(ropeLengthsArray);
+    const resultDiv = document.getElementById("result");
+    resultDiv.innerText = "Minimum cost of ropes: " + minCost;
+}
+
+function calculateMinimumCost(ropeLengths) {
+    // Implementing the minimum cost calculation logic
+    ropeLengths.sort((a, b) => a - b);
+    let totalCost = 0;
+
+    while (ropeLengths.length > 1) {
+        const newRope = ropeLengths.shift() + ropeLengths.shift();
+        totalCost += newRope;
+        ropeLengths.push(newRope);
+        ropeLengths.sort((a, b) => a - b);
+    }
+
+    return totalCost;
+}
+
